acd5e972d2fbb32212db230187d56ef08d650904
wangyao <15924253943@163.com>
2026-06-04 09:49:58 +0800

    fix: 移除列功能按钮多余dropdown样式，统一按钮布局

---
+++ server%2Fconfig.py
@@ -0,0 +1,13 @@
+"""
+MySQL 连接配置
+"""
+DB_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "",
+    "database": "data_ledger",
+    "charset": "utf8mb4"
+}
+
+FLASK_SECRET = "data-ledger-dev-2026"
